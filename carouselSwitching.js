1e8adbea2531b600dddfa1a73b385aead955dcca
Jakub <kubaflor23@gmail.com>
2021-10-24 21:42:31 +0200

    Added animation to app pages

--- carouselSwitching.js
+++ carouselSwitching.js
@@ -1,11 +1,12 @@
 let appName = document.getElementById("carouselSwitching").getAttribute("data-appName");
 
-var screenshots = [``];
+let screenshots = [``];
+let subtitle = ""
 
 switch (appName) {
-    case `tarbus`: screenshots = [`iPhone.png`, `iPhone.png`, `iPhone.png`]; break;
-    case `whatABrick`: screenshots = [`iPhone.png`, `iPhone.png`, `iPhone.png`]; break;
-    case `foodtracker`: screenshots = [`iPhone.png`, `iPhone.png`, `iPhone.png`]; break;
+    case `tarbus`: screenshots = [`iPhone.png`, `iPhone.png`, `iPhone.png`]; subtitle = "Your pocket bus timetable"; break;
+    case `What A Brick`: screenshots = [`iPhone.png`, `iPhone.png`, `iPhone.png`]; subtitle = "Identify your brick with AI!"; break;
+    case `foodtracker`: screenshots = [`iPhone.png`, `iPhone.png`, `iPhone.png`]; subtitle = "Everything about foodtrucks!"; break;
     default: screenshots = []; break;
 }
 
@@ -18,8 +19,10 @@ $(window).on(`resize`, function() {
 function setScreenshots() {
     let win = $(this);
     if (win.width() < 768) {
-        $(`#screenshots`).html(`<div class="row"><div class="col-sm-12 col-md-6 align-content-center bigScreenshot pt-3 pt-md-0">\n` +
-            `                    <div id="carouselControls" class="carousel carousel-dark slide" data-bs-ride="carousel">\n` +
+        $(`#screenshots`).html(`<div class="row"><div class="col-sm-12 col-md-6 align-content-center bigScreenshot bigHeader pt-3 pt-md-0">\n` +
+            `                    <h1 class="elementToFadeAndSlideIn1">Wonderful icons</h1>`+
+            `                    <h5 class="elementToFadeAndSlideIn2">WWDC21 entry</h5>`+
+            `                    <div id="carouselControls" class="carousel carousel-dark slide elementToFadeAndSlideIn3" data-bs-ride="carousel">\n` +
             `                        <div class="carousel-inner">\n` +
             `                            <div class="carousel-item active">\n` +
             `                                <img src="/img/${screenshots[0]}" alt="tarBUS">\n` +
@@ -44,10 +47,11 @@ function setScreenshots() {
     }
     else
     {
-        $(`#screenshots`).html(`<div class="row">\n` +
-            `                        <div class="col-md-4 bigScreenshot"><img src="../img/${screenshots[0]}" alt="tarBUS"></div>\n` +
-            `                        <div class="col-md-4 bigScreenshot"><img src="../img/${screenshots[1]}" alt="tarBUS"></div>\n` +
-            `                        <div class="col-md-4 bigScreenshot"><img src="../img/${screenshots[2]}" alt="tarBUS"></div>\n` +
+        $(`#screenshots`).html(`<div class="row bigHeader">\n` +
+            `                        <div class="col-12a mb-3"><h1 class="elementToFadeAndSlideIn1">${appName}</h1><h5 class="elementToFadeAndSlideIn2">${subtitle}</h5></div>`+
+            `                        <div class="col-md-4 bigScreenshot elementToFadeAndSlideIn3"><img src="../img/${screenshots[0]}" alt="tarBUS"></div>\n` +
+            `                        <div class="col-md-4 bigScreenshot elementToFadeAndSlideIn3"><img src="../img/${screenshots[1]}" alt="tarBUS"></div>\n` +
+            `                        <div class="col-md-4 bigScreenshot elementToFadeAndSlideIn3"><img src="../img/${screenshots[2]}" alt="tarBUS"></div>\n` +
             `                    </div>`);
     }
 }
